4880f7e8abe890e8628f31e1c56edbb7d3fe4ad9
John Hawkins <john@vegasgeek.com>
2023-12-01 11:35:21 -0800

    day 1 part1

---
+++ 01/day01-1.php
@@ -0,0 +1,23 @@
+<?php
+// Day 1.
+
+$number = 0;
+$file   = fopen( 'data.txt', 'r' );
+if ( ! $file ) {
+	exit;
+}
+
+while ( ! feof( $file ) ) {
+	$line = trim( fgets( $file, 1024 ) );
+
+	$digits = filter_var( $line, FILTER_SANITIZE_NUMBER_INT );
+
+	$first = substr( $digits, 0, 1 );
+	$last  = substr( $digits, -1 );
+
+	$number += intval( $first . $last );
+
+}
+fclose( $file );
+
+echo $number;
