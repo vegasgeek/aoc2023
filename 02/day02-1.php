d4a659279a692fecf4bc683da2fabe77c7386982
John Hawkins <john@vegasgeek.com>
2023-12-02 10:32:18 -0800

    day 2, but not quite working

---
+++ 02/day02-1.php
@@ -0,0 +1,62 @@
+<?php
+// Day 2 - Part 1.
+// Do you want to play a game?
+
+
+$file = fopen( 'data.txt', 'r' );
+if ( ! $file ) {
+	exit;
+}
+$removes = array( ' ', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0' );
+
+$limit = array(
+	'red'   => 12,
+	'green' => 13,
+	'blue'  => 14,
+);
+
+$parts = array();
+while ( ! feof( $file ) ) {
+	$game                = trim( fgets( $file, 1024 ) );
+	$parts               = explode( ':', $game );
+	$dice_count          = array();
+	$dice_count['blue']  = 0;
+	$dice_count['green'] = 0;
+	$dice_count['red']   = 0;
+
+	$game_num = intval( filter_var( $parts[0], FILTER_SANITIZE_NUMBER_INT ) );
+
+	$round = explode( ';', $parts[1] );
+
+	foreach ( $round as $key => $value ) {
+		$dice = explode( ',', $value );
+
+		foreach ( $dice as $k => $v ) {
+			$num   = intval( filter_var( $v, FILTER_SANITIZE_NUMBER_INT ) );
+			$color = trim( str_replace( $removes, '', $v ) );
+
+			$dice_count[ $color ] += $num;
+		}
+	}
+
+	if ( ( $dice_count['blue'] > $limit['blue'] ) || ( $dice_count['green'] > $limit['green'] ) || ( $dice_count['red'] > $limit['red'] ) ) {
+		// echo 'Game ' . $game_num . ' is invalid.' . "\n";
+	} else {
+		echo 'Game ' . $game_num . ' is valid.' . "\n";
+		$number += intval( $game_num );
+	}
+}
+
+fclose( $file );
+
+echo $number;
+
+/**
+ * Status: Failing
+ * I'm getting an answer of 309.
+ * I'm showing the following games as valid:
+ * 12, 29, 43, 55, 77, 93
+ * total those up and it equals 309.
+ *
+ * Not sure what I'm missing.
+ */
