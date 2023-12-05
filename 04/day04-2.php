bc3679d0a8b360e4bafd3d4495dd06eb295aabf9
John Hawkins <john@vegasgeek.com>
2023-12-04 20:56:49 -0800

    Fixed it. Part 2 solved.

--- 04/day04-2.php
+++ 04/day04-2.php
@@ -1,8 +1,8 @@
 <?php
-// Day 4 - Part 1.
+// Day 4 - Part 2.
 // Scratch and Win!
 
-$file = fopen( 'data-test.txt', 'r' );
+$file = fopen( 'data.txt', 'r' );
 if ( ! $file ) {
 	exit;
 }
@@ -25,64 +25,29 @@ foreach ( $line as $key => $value ) {
 	$winners = explode( ' ', $set[0] );
 	$numbers = explode( ' ', $set[1] );
 
-	// remove any array elements that are non-numeric.
 	$winners        = array_filter( $winners, 'is_numeric' );
 	$numbers        = array_filter( $numbers, 'is_numeric' );
 	$matches        = array_intersect( $winners, $numbers );
-	$cards[ $card ] = count( $matches );
-
-	$copies[ $card ]      = 0;
-	$total_cards[ $card ] = 1;
+	$cards[ $card ] = array(
+		'matches' => count( $matches ),
+		'total'   => 1,
+	);
 }
 
-// making copies!
-foreach ( $cards as $card => $matches ) {
-	echo '----- Start card ' . $card . ' -----' . "\n";
+foreach ( $cards as $card => $value ) {
+	$matches   = $value['matches'];
+	$total     = $value['total'];
 	$next_card = $card + 1;
 
-	if ( $matches > 0 ) {
-		$loop = $matches;
-		echo 'Card ' . $card . ' has ' . $matches . ' matches.' . "\n";
-		while ( $loop > 0 ) {
-			$copies[ $next_card ]      = $copies[ $next_card ] + 1;
-			$total_cards[ $next_card ] = $total_cards[ $next_card ] + 1;
-			$loop--;
-			$next_card++;
-		}
-	}
-
-	echo '------ Start copies ' . $card .' ------' . "\n";
-	if ( $copies[ $card ] > 0 && $matches > 0 ) {
-		$loop = $copies[ $card ];
-		echo 'Card ' . $card . ' has ' . $copies[ $card ] . ' copies.' . "\n";
-		$next_card = $card + 1;
-
-		while ( $loop > 0 ) {
-			while ( $matches > 0 ) {
-				$copies[ $next_card ] = $copies[ $next_card ] + 1;
-				$total_cards[ $next_card ] = $total_cards[ $next_card ] + 1;
-				$matches--;
-				$next_card++;
-			}
-			$loop--;
-		}
+	for ( $i = 0; $i < $matches; $i++ ) {
+		$cards[ $next_card ]['total'] += $cards[ $card ]['total'];
+		$next_card++;
 	}
 }
 
-print_r( $copies );
+$total = 0;
+foreach ( $cards as $card => $value ) {
+	$total += $value['total'];
+}
 
-
-// print_r( $total_cards );
-// print_r( $copies );
-// echo $total_tickets . "\n";
-
-/**
-
-1 . 1
-2 .. 2
-3 .... 4
-4 ........ 8
-5 .............. 14
-6 . 1
-
- */
+echo $total;
