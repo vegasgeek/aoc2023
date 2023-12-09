ad98f5827d338591cf70bf6498e7a4f6afdd5821
John Hawkins <john@vegasgeek.com>
2023-12-09 09:50:41 -0800

    I’m an idiot.

--- 08/day08-1.php
+++ 08/day08-1.php
@@ -13,53 +13,28 @@ while ( ! feof( $file ) ) {
 	$line[] = trim( fgets( $file, 1024 ) );
 }
 
-$map_l = array();
+$start  = 'AAA';
+$target = 'ZZZ';
+$map    = array();
 
 foreach ( $line as $key => $value ) {
 	$data = explode( ' = ', $value );
-
 	$loc   = $data[0];
 	$left  = substr( $data[1], 1, 3 );
 	$right = substr( $data[1], 6, 3 );
 
-	if ( ! isset( $start_l ) ) {
-		$start_l = $loc;
-	}
-
-	$map_l[ $loc ] = array(
+	$map[ $loc ] = array(
 		'left'  => $left,
 		'right' => $right,
 	);
 }
 
-$ord_l = str_split( 'LLRLLRRLRLRRRLRRLLRRRLRLRLRRLRRRLRRLRLRLLRLLLRRRLRRLRRRLRRRLRRRLRLRRLLRRLRRLRRLRRRLRLRRRLLRLRRLRRRLRLRRRLRRRLRLRRRLLRRRLRRRLRLRRLRLRRRLLRRLRRLRRLRRLRLRLRRRLLRRRLRRLRRRLRLRLRRRLLRLRRLLRLRRLRLRRRLRLRRLLRRRLLRRLRLRLLRLLRRLRRLLRRLRLRRLRLRLRRRLRRLRLLLLRRLRLRLRRRLLLRRRLRRLRRLRLLRLRRRLLLRRRLRRRLRRRR' );
-
-// test data //
-$start_t = 'AAA';
-$map_t = array();
-$map_t[ 'AAA' ] = [ 'left' => 'BBB', 'right' => 'BBB' ];
-$map_t[ 'BBB' ] = [ 'left' => 'AAA', 'right' => 'ZZZ' ];
-$map_t[ 'ZZZ' ] = [ 'left' => 'ZZZ', 'right' => 'ZZZ' ];
-
-$ord_t = str_split( 'LLR' );
-///////////////
-
-$use = 'l';
-
-if ( 'l' === $use ) {
-	$start = $start_l;
-	$map   = $map_l;
-	$ord   = $ord_l;
-} else {
-	$start = $start_t;
-	$map   = $map_t;
-	$ord   = $ord_t;
-}
+$ord = str_split( 'LLRLLRRLRLRRRLRRLLRRRLRLRLRRLRRRLRRLRLRLLRLLLRRRLRRLRRRLRRRLRRRLRLRRLLRRLRRLRRLRRRLRLRRRLLRLRRLRRRLRLRRRLRRRLRLRRRLLRRRLRRRLRLRRLRLRRRLLRRLRRLRRLRRLRLRLRRRLLRRRLRRLRRRLRLRLRRRLLRLRRLLRLRRLRLRRRLRLRRLLRRRLLRRLRLRLLRLLRRLRRLLRRLRLRRLRLRLRRRLRRLRLLLLRRLRLRLRRRLLLRRRLRRLRRLRLLRLRRRLLLRRRLRRRLRRRR' );
 
 $data = doloop( $start, 0 );
 
 $loop_count = 0;
-while ( 'ZZZ' !== $data['loc'] ) {
+while ( $data['target'] !== $data['loc'] ) {
 	$loop_count++;
 	$start = $data['loc'];
 	$step  = $data['step'];
@@ -67,12 +42,10 @@ while ( 'ZZZ' !== $data['loc'] ) {
 }
 
 function doloop( $start, $step_count ) {
-	global $ord, $map;
+	global $ord, $map, $target;
 	$loc = $start;
 	foreach ( $ord as $key => $value ) {
 		$step_count++;
-
-		echo $step_count . ': ' . $loc . ' ' . $map[ $loc ]['left'] . ' ' . $map[ $loc ]['right'] . ' ' . $value;
 
 		if ( 'L' === $value ) {
 			$loc = $map[ $loc ]['left'];
@@ -80,9 +53,6 @@ function doloop( $start, $step_count ) {
 			$loc = $map[ $loc ]['right'];
 		}
 
-		echo ' | Found: ' . $loc . "\n";
-
-		$target = 'ZZZ';
 		if ( $target === trim( $loc ) ) {
 			echo 'Found '. $target . ' in ' . $step_count . ' steps' . "\n";
 			exit;
@@ -91,5 +61,6 @@ function doloop( $start, $step_count ) {
 	return array(
 		'loc' => $loc,
 		'step' => $step_count,
+		'target' => $target,
 	);
 }
